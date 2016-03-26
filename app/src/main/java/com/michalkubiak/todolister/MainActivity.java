c9930b9cbe3bf7874982bf7643cdabfe10c6da27
Michal <kubiakmichal@ymail.com>
2016-03-26 19:57:49 +0100

    added theme changing on tab change (needs improvement)

--- app/src/main/java/com/michalkubiak/todolister/MainActivity.java
+++ app/src/main/java/com/michalkubiak/todolister/MainActivity.java
@@ -1,6 +1,7 @@
 package com.michalkubiak.todolister;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
@@ -10,8 +11,10 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,9 +27,6 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private FloatingActionButton fab;
-    private MeetingListFragment meetingListFragment;
-    private ReminderListFragment reminderListFragment;
-    private ShoppingListFragment shoppingListFragment;
     private FragmentManager fragmentManager;
 
     private int[] tabIcons = {
@@ -39,19 +39,71 @@ public class MainActivity extends AppCompatActivity {
     private String meetingListTag;
     private String reminderListTag;
 
+    private int currentTab = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        if (getIntent().hasExtra("TAG")) {
+
+            currentTab = getIntent().getIntExtra("TAG", 0);
+            Log.v("MOJTAG", "The state of saved instace is: " + currentTab);
+
+            switch (currentTab) {
+                case 0:
+                    setTheme(R.style.MyMaterialTheme_Red);
+                    break;
+                case 1:
+                    setTheme(R.style.MyMaterialTheme_Green);
+                    break;
+                case 2:
+                    setTheme(R.style.MyMaterialTheme_Blue);
+                    break;
+                default:
+                    setTheme(R.style.MyMaterialTheme_Blue);
+            }
+
+        }
+
+
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
+        viewPager.setCurrentItem(currentTab);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                currentTab = position;
+                Intent intent = getIntent();
+                intent.putExtra("TAG", currentTab);
+                finish();
+
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+/*
+                Toast.makeText(getApplicationContext(),
+                        "Current tab is: " + position, Toast.LENGTH_SHORT).show();*/
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
@@ -59,6 +111,7 @@ public class MainActivity extends AppCompatActivity {
         setupTabIcons();
 
         fab = (FloatingActionButton) findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -131,6 +184,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
             Fragment createdFragment = (Fragment) super.instantiateItem(container, position);
+
             // get the tags set by FragmentPagerAdapter
             switch (position) {
                 case 0:
@@ -140,7 +194,7 @@ public class MainActivity extends AppCompatActivity {
                     meetingListTag = createdFragment.getTag();
                     break;
                 case 2:
-                    reminderListTag =createdFragment.getTag();
+                    reminderListTag = createdFragment.getTag();
                     break;
 
             }
@@ -161,5 +215,12 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             return mFragmentTitleList.get(position);
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt("TAB", currentTab);
+        super.onSaveInstanceState(outState);
+
     }
 }
